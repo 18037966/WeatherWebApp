bc00cac0d4cb9deb94913821eb3e103447b151ca
Zubaiyer Karim Labib <zbaiyer.labib@gmail.com>
2017-11-10 00:31:34 +1100

    Second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '2c61dc8aac9985bd62374c0af4a39d93';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
